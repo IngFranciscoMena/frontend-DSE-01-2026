cc1e509789e8de7f42cbe73f2475e1e50ee7f63b
IngFranciscoMena <franciscodaniel.penate@unab.edu.sv>
2026-04-15 10:40:14 -0600

    se agrega desarrollo de listado de clientes y formulario para creación y edición de clientes

---
+++ src/app/features/clientes/services/clientes.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ClientesService {
+  clientes = [
+    { codigo: 1, nombre: 'Juan Pérez', dui: '00000000-0', telefono: '1234-5678', estado: 'Activo' },
+    { codigo: 2, nombre: 'María López', dui: '11111111-1', telefono: '2345-6789', estado: 'Inactivo' },
+    { codigo: 3, nombre: 'Carlos Gómez', dui: '22222222-2', telefono: '3456-7890', estado: 'Activo' },
+  ];
+
+  getClientes(){
+    return this.clientes;
+  }
+
+  saveCliente(cliente: any){
+    // lógica 
+    const nuevoCodigo = this.clientes.length > 0 ? Math.max(...this.clientes.map(c => c.codigo)) + 1 : 1  // Spread Operator
+
+    cliente.codigo = nuevoCodigo;
+
+    this.clientes.push(cliente);
+  }
+
+
+}
